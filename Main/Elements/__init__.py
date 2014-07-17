c2b5f20404bfc1956ee3e890d0e79107cefa935a
artur <artur.kulmukhametov@gmail.com>
2014-07-17 16:45:18 +0200

    first commit

---
+++ Main/Elements/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'artur'
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class Property:
+    """ This is an abstract class with the following fields and methods:
+
+    """
---
+++ Test/test_cat.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+__author__ = 'artur'
+
+
+class TestCat(TestCase):
+    def test_make_sound(self):
+        assert(1,1)
